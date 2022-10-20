ffe35fd73ff90b2d270b212f66d62484774540dd
babailan <babailanxx@gmail.com>
2022-10-21 01:37:37 +0800

    harden james 3 pointers

--- src/components/sign-up/sign-up.tsx
+++ src/components/sign-up/sign-up.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
-
+import BackgroundImage from '../../images/simpleb.svg'
+import style from './style.module.scss';
 function SignUp() {
     const [name, setName] = useState<string>("")
     const [password, setPassword] = useState<string>("")
@@ -9,13 +11,16 @@ function SignUp() {
         setter(element.value);
     };
 
-    return (<>
-        <div>
+    return (<div className={style.container} >
+        <div className={style.backgroundImage}>
+            <Image src={BackgroundImage} layout={"fill"} objectFit={"cover"} />
+        </div>
+        <div className={style.content}>
             <input placeholder="name" onChange={(e) => stateChange(e, setName)}></input>
             <input placeholder="password" onChange={(e) => stateChange(e, setPassword)}></input>
-            <div>submit</div>
+            <div className={style.submit}>submit</div>
         </div>
-    </>)
+    </div>)
 }
 
 
